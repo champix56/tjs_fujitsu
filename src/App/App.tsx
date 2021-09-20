fda77c57c837addac777d025cd4b11cb4740658f
alex <desorbaix@free.fr>
2021-09-20 12:21:12 +0200

    MEO 1 er cmp

---
+++ src/App/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Button from './components/Button/Button';
+
+function App() {
+  return (
+    <div className="App">
+      DEMAT breizh
+      <hr/>
+      <Button/> <Button/>
+      <Button/>
+    </div>
+  )
+}
+
+export default App;
