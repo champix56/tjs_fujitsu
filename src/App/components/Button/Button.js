36e7d8aafd0abc165019d88062660f95a6ac5230
alex <desorbaix@free.fr>
2021-09-20 14:23:41 +0200

    props types & defult props

--- src/App/components/Button/Button.js
+++ src/App/components/Button/Button.js
@@ -1,10 +1,19 @@
 import React from "react";
 import "./Button.css";
+import PropTypes from 'prop-types'
 function Button(props) {
   console.log(props);
-  return <button className="Button">{
-      props.children?props.children:props.text
-  }</button>;
+  return <button className="Button" onClick={()=>{
+    alert('on a clicker sur benjamin')
+  }}>{props.children}</button>;
 }
+Button.propTypes={
+  children : PropTypes.any.isRequired,
+  onLeftClick: PropTypes.func.isRequired,
+}
+Button.defaultProps={
+  children:'Hello ... de lu',
+  onLeftClick:()=>{  }
 
+}
 export default Button;
