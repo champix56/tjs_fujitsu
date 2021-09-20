497108284123b66ef32f751bbb0f75cdbfb2a210
alex <desorbaix@free.fr>
2021-09-20 15:18:29 +0200

    demonstration des unions class css

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import './App.css';
 import Button from './components/Button/Button';
+import Button2 from './components/Buttonts/Button';
 
 function App() {
   return (
     <div className="App">
       DEMAT breizh
       <hr/>
-      <Button onLeftClick={(arg)=>{
-        console.log(arg);
-      }}>
+      <Button onLeftClick={(evt)=>{
+        console.log(evt);
+      }} backgroundColor="tomato">
         <div>Mon button</div>
       </Button> 
       <Button/>
-      <Button >Demat Bretagne</Button>
-      <Button onLeftClick={()=>{}} />
+      <Button2 >Demat Bretagne</Button2>
+      <Button  backgroundColor="white" color="darkblue" style={{textDecoration:'underline', fontStyle:'italic'}}  >Demat Bretagne</Button>
+      <Button backgroundColor="yellowgreen" onLeftClick={()=>{}} />
     </div>
   )
 }
