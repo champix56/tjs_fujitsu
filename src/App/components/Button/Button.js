36287be15b9dab84e408517a8b87d532593f59a3
alex <desorbaix@free.fr>
2021-09-20 15:18:09 +0200

    spread de style

--- src/App/components/Button/Button.js
+++ src/App/components/Button/Button.js
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Button.css";
-import PropTypes from 'prop-types'
-function Button(props) {
-  console.log(props);
-  return <button className="Button" onClick={()=>{
-    alert('on a clicker sur benjamin')
-  }}>{props.children}</button>;
-}
-Button.propTypes={
-  children : PropTypes.any.isRequired,
-  onLeftClick: PropTypes.func.isRequired,
-}
-Button.defaultProps={
-  children:'Hello ... de lu',
-  onLeftClick:()=>{  }
+import PropTypes from "prop-types";
 
+function Button(props) {
+  const [clicked, setClicked] = useState(false);
+  console.log(props);
+  return (
+    <button
+      className={clicked ? "Button clicked" : "Button"}
+      onClick={(evt)=>{
+        setClicked(true);
+        props.onLeftClick(evt);
+        setTimeout(()=>{setClicked(false)},300);
+      }}
+      type={props.type}
+      style={ {...props.style,backgroundColor:props.backgroundColor, color:props.color} }
+    >
+      {props.children}
+    </button>
+  );
 }
+Button.propTypes = {
+  children: PropTypes.any.isRequired,
+  onLeftClick: PropTypes.func.isRequired,
+  type:PropTypes.string,
+  backgroundColor:PropTypes.string.isRequired,
+  color: PropTypes.string,
+  style:PropTypes.object,
+};
+Button.defaultProps = {
+  children: "Hello ... de lu",
+  onLeftClick: () => {},
+  backgroundColor:'green',
+  color:'white'
+};
 export default Button;
