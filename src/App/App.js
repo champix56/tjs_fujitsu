6a1f43b0fead4a899cc027f69e180f28903f2755
alex <desorbaix@free.fr>
2021-09-22 12:01:14 +0200

    suppr. de l'etat de App

--- src/App/App.js
+++ src/App/App.js
@@ -2,32 +2,13 @@ import React from "react";
 import styles from "./App.module.css";
 import Navbar from "./components/Navbar/Navbar";
 import MemeCreator from "./pages/MemeCreator/MemeCreator";
-import store, { currentInitialState, memeInitialState } from "./store/store";
-const initialState = {
-  current: currentInitialState,
-  images: memeInitialState.images
-};
-
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = initialState;
-  }
-  componentDidMount() {
-    this.setState({ current: store.getState().current, images:store.getState().lists.images });
-    store.subscribe(() => {
-      this.setState({ current: store.getState().current, images:store.getState().lists.images });
-    });
-  }
   render() {
     return (
       <>
         <div className={styles.App}>
           <Navbar/>
-
-
           <MemeCreator/>
-        
         </div>
         <div>{JSON.stringify(this.state)}</div>
       </>
